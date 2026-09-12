8d2492064f7fc4ae466d79ff4eaa97b75d88e256
itspushpank <itspushpank35@gmail.com>
2026-09-12 23:24:37 +0530

    Add HeroSection and Animated components for enhanced UI and animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // import React from 'react'
 
 import Navbar from "./components/Navbar"
+import HeroSection from "./sections/HeroSection"
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <HeroSection />
     </>
   )
 }
